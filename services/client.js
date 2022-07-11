1ee94d3d6a6d42c4309ca1063c59cf4f936a2a59
Dave Fisher <dave.j.fish@gmail.com>
2022-07-11 14:43:58 -0700

    basic html/css outlines finished

--- services/client.js
+++ services/client.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_URL = 'https://lbhcxvyspdaifxljifnq.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiaGN4dnlzcGRhaWZ4bGppZm5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY1NDE1OTYsImV4cCI6MTk3MjExNzU5Nn0.CYqr69yW5FRoOjB1MgOtYhQ3GS8oEGzFi8VF0D1tAKw';
 
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
