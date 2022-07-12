12b99134cac3d0db965410f3db8eb7a3147475e7
Dave Fisher <dave.j.fish@gmail.com>
2022-07-12 12:43:13 -0700

    basic drag functionality added

--- components/Prefectures.js
+++ components/Prefectures.js
@@ -23,10 +23,19 @@ function addPrefecture({ prefecture, handleAddRice, handleDeleteRice }) {
     const ul = document.createElement('ul');
     ul.classList.add('rice-list');
 
+    ul.addEventListener('dragenter', dragEnter);
+    ul.addEventListener('dragover', dragOver);
+    ul.addEventListener('dragleave', dragLeave);
+    ul.addEventListener('drop', drop);
+
     for (let rice of prefecture.kome) {
         let li = document.createElement('li');
         li.classList.add('rice-box');
         li.textContent = rice.riceName;
+        li.draggable = true;
+        li.setAttribute('id', `${rice.id}`);
+
+        li.addEventListener('dragstart', dragStart);
 
         li.addEventListener('click', () => {
             handleDeleteRice(rice);
@@ -65,4 +74,44 @@ function updateRice({ prefecture, handleAddRice }) {
     form.append(label);
 
     return form;
+}
+
+function dragStart(e) {
+    // console.log('we draggin stuff');
+    // console.log('target id: ', e.target.id);
+    e.dataTransfer.setData('text/plain', e.target.id);
+    setTimeout(() => {
+        e.target.classList.add('hidden');
+    }, 0);
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('drag-over');
+}
+
+function drop(e) {
+    e.target.classList.remove('drag-over');
+
+    // get draggable element
+    const id = e.dataTransfer.getData('text/plain');
+    const draggable = document.getElementById(id);
+
+    // add it to the drop target
+    if (e.target.tagName === 'UL') {
+        e.target.appendChild(draggable);
+    }
+
+
+    // display draggable element
+    draggable.classList.remove('hidden');
 }
