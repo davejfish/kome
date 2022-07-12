6b2ae9bb4c4486ae000a82240aa5ceefb86b92fe
Dave Fisher <dave.j.fish@gmail.com>
2022-07-11 21:45:06 -0700

    adding new rice via form working

--- rice/rice.js
+++ rice/rice.js
@@ -1,8 +1,9 @@
 import { getUser, signOut } from '../services/auth-service.js';
-import { protectPage, findByID } from '../utils.js';
+import { findByID, protectPage } from '../utils.js';
 import createUser from '../components/User.js';
 import createRiceList from '../components/riceList.js';
-import { addRice, deleteRice, getRicePrefecture } from '../services/client.js';
+import createForm from '../components/form.js';
+import { addRice, deleteRice, getPrefectures, getRicePrefecture } from '../services/client.js';
 
 // State
 import state from '../state.js';
@@ -13,16 +14,20 @@ async function handlePageLoad() {
     protectPage(state.user);
 
     state.rice = await getRicePrefecture();
+    state.prefectures = await getPrefectures();
 
     display();
 }
 
 async function handleAddRice(riceName, prefID) {
-    const newRice = await addRice(riceName, prefID);
+    const newRice = await addRice(
+        riceName,
+        prefID
+    );
 
-    const pref = findByID(state.prefectures, prefID);
-
-    pref.kome.push(newRice);
+    const prefectures = findByID(state.prefectures, Number(prefID));
+    newRice.prefectures = prefectures;
+    state.rice.unshift(newRice);
 
     display();
 }
@@ -51,9 +56,13 @@ const User = createUser(
 
 const RiceList = createRiceList(document.querySelector('.rice-table'), handleDeleteRice);
 
+const CreateForm = createForm(document.querySelector('.add-rice'), handleAddRice);
+
 function display() {
     User({ user: state.user });
     RiceList({ rice: state.rice });
+    CreateForm();
+
 }
 
 handlePageLoad();
